bb6aa4e883c8ef2026a243da77e63d91a28c08f6
dsw333 <65722424+dsw333@users.noreply.github.com>
2020-06-14 14:05:17 -0400

    updated first draft with plotly bar graph
    This work still needs: #Make horizontal
    #Add title
    #Capitalize axis
    #Include Total Amunt

--- exec_dash.py
+++ exec_dash.py
@@ -19,3 +19,22 @@ print("-----------------------")
 print("VISUALIZING THE DATA...")
 
 
+#Total monthly sales for the business, equivalent to the sum of total monthly sales for each product, formatted as USD with a dollar sign and two decimal places 
+#A list of the top selling products, and total monthly sales for each, formatted as USD with a dollar sign and two decimal places
+#At least one chart or graph depicting this or related information to support the project objectives. Chart titles should include a human-friendly textual representation of the selected month and year 
+
+
+
+import pandas as pd
+url = 'https://raw.githubusercontent.com/dsw333/exec-dash/master/sales-201904.csv'
+df = pd.read_csv(url, error_bad_lines=False)
+import plotly.express as px
+#df = pd.read_csv("sales-201904.csv")
+fig = px.bar(df, x = 'product', y = 'sales price')
+fig.show()
+
+
+#Make horizontal
+#Add title
+#Capitalize axis
+#Include Total Amunt
