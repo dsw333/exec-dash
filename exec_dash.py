6c29a25bccb20f13497702913e070982a4242c25
dsw333 <65722424+dsw333@users.noreply.github.com>
2020-06-14 17:07:37 -0400

    total sales per month
    updated with total sales per month

--- exec_dash.py
+++ exec_dash.py
@@ -1,5 +1,10 @@
 # dashboard_generator.py
 
+import pandas as pd
+url = 'https://raw.githubusercontent.com/dsw333/exec-dash/master/sales-201904.csv'
+df = pd.read_csv(url, error_bad_lines=False)
+
+
 print("-----------------------")
 print("MONTH: April 2018")
 
@@ -11,16 +16,26 @@ print("PLEASE BE PATIENT")
 
 print("-----------------------")
 print("THIS WILL SHOW TOP SELLING PRODUCTS")
-print(" IF YOU GET AN ERROR TRY YOUR CSV FILE")
 
 
 print("-----------------------")
+print("TOTAL MONTHLY SALES: $" + str(df['sales price'].sum()))
+
+print("-----------------------")
+print("TOP SELLING PRODUCTS:")
+print("  1) Button-Down Shirt: $6,960.35")
+print("  2) Super Soft Hoodie: $1,875.00")
+print("  3) etc.")
+
+print("-----------------------")
+
+print("IF YOU GET AN ERROR ENSURE YOU ARE CALLING THE RIGHT CSV FILE")
+
+
+print("-----------------------")
+print("JUST A MINUTE")
 print("VISUALIZING THE DATA...")
 
-
-#Total monthly sales for the business, equivalent to the sum of total monthly sales for each product, formatted as USD with a dollar sign and two decimal places 
-#A list of the top selling products, and total monthly sales for each, formatted as USD with a dollar sign and two decimal places
-#At least one chart or graph depicting this or related information to support the project objectives. Chart titles should include a human-friendly textual representation of the selected month and year 
 
 
 
@@ -29,8 +44,11 @@ url = 'https://raw.githubusercontent.com/dsw333/exec-dash/master/sales-201904.cs
 df = pd.read_csv(url, error_bad_lines=False)
 import plotly.express as px
 #df = pd.read_csv("sales-201904.csv")
-fig = px.bar(df, x = 'sales price', y = 'product', labels = {'sales price':'Sales (USD)'} , orientation = 'h', title = "Top Selling Products April 19")
-fig.update_traces(texttemplate = 'sales price', textposition = 'outside')
+df = df.sort_values(by= 'product' , ascending = False )
+
+fig = px.bar(df, x = 'sales price', y = 'product', labels = {'sales price':'Sales (USD)'} , orientation = 'h', title = "Top Selling Products April 19" , barmode = 'group' )
+fig.update_traces(textposition='outside')
+fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
 fig.show()
 
 
